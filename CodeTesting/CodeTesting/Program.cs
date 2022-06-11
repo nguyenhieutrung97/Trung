1f4c7b18c1095e22a3f1f9ab355656c5f146839b
nguyenhieutrung97 <30740080+nguyenhieutrung97@users.noreply.github.com>
2022-06-11 20:18:50 +0700

    Initial Project

---
+++ CodeTesting/CodeTesting/Program.cs
@@ -0,0 +1,25 @@
+﻿public interface IStringHandler
+{
+	string ConvertVerboseStringToRawString(string input);
+}
+public class StringHandler : IStringHandler
+{
+	public string ConvertVerboseStringToRawString(string input)
+	{
+		if (input[0] == input[input.Length - 1] && input[0] == '\'')
+		{
+			input = input.Substring(1, input.Length - 2);
+		}
+		return input;
+	}
+}
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Please fill the string you want convert:");
+		var input = Console.ReadLine();
+		var stringHandler = new StringHandler();
+		Console.WriteLine(stringHandler.ConvertVerboseStringToRawString(input));
+	}
+}
